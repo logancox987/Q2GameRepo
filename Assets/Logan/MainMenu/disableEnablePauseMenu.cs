c05ee889420c00a78cd3e7ce746b3770fd6881f9
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2021-12-07 10:10:50 -0800

    Pause Menu Implementation

---
+++ Assets/Logan/MainMenu/disableEnablePauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableEnablePauseMenu : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject pauseMenuCanvas;
+    public float menuCooldown;
+    public float cooldownTimer;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pauseMenuCanvas = GameObject.FindGameObjectWithTag("PauseMenuCanvas");
+        pauseMenuCanvas.SetActive(false);
+        cooldownTimer = menuCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cooldownTimer -= Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Escape) && cooldownTimer < .1f)
+        {
+            if(pauseMenuCanvas.activeInHierarchy == false)
+            {
+                pauseMenuCanvas.SetActive(true);
+            }
+            else
+            {
+                pauseMenuCanvas.SetActive(false);
+                cooldownTimer = menuCooldown;
+            }
+        }
+
+        if(pauseMenuCanvas.activeInHierarchy == true)
+        {
+            Time.timeScale = 0f;
+            player.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            player.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuCanvas.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Logan/MainMenu/menuButtonActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuButtonActions : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void quitQame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }    
+}
