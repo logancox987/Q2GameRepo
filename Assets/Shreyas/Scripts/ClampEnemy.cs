4ebaa3d8cd306d565584cb06bce0fd1f5dcb9ea9
MUKILTEO\1710136 <1710136@mukilteo.wednet.edu>
2021-12-15 08:20:03 -0800

    Enemy Implementation

---
+++ Assets/Shreyas/Scripts/ClampEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampEnemy : MonoBehaviour
+{
+    private Enemy_Behaviour enemyParent;
+    public Transform leftBound;
+    public Transform rightBound;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Clamp(transform.position.x, leftBound.position.x, rightBound.position.x);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+    }
+}
+
+
