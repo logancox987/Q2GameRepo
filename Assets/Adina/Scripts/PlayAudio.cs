f6b87efdf5701678e6b5282a8b319f82719162ba
MUKILTEO\1710275 <1710275@mukilteo.wednet.edu>
2022-01-14 09:32:33 -0800

    Trying

---
+++ Assets/Adina/Scripts/PlayAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource[] audioSourceArray;
+    public AudioClip[] audioClipArray;
+
+    double nextStartTime;
+    int toggle;
+    int nextClip;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(AudioSettings.dspTime > nextStartTime - 1)
+        {
+            AudioClip clipToPlay = audioClipArray[nextClip];
+            //Loads the
+        }
+    }
+}
